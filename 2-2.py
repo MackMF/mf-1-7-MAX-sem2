2201757bf5f8fb290ae7511bce45967dd0f8bee5
MackMF <160236514+MackMF@users.noreply.github.com>
2024-03-15 15:30:45 +0300

    Update 2-2.py

--- 2-2.py
+++ 2-2.py
@@ -1,1 +1,20 @@
-
+import math
+t  = float(input('введите t:'))
+a = (t-5)
+b = -3*t
+c = 2*(t-3)
+if a == 0:
+    x =  -c/b
+    print('имее т один корень x =', x)
+else:
+    D = b**2 - 4 * a * c
+    print("D:", D)
+    print("a:", a)
+    print("b:", b)
+    print("c:", c)
+    if D>=0:
+        x1 = (-b + math.sqrt(D)) / (2* a)
+        x2 = (-b - math.sqrt(D)) / (2* a)
+        print('x1 =', x1, '\nx2 =', x2)
+    else:
+        print("не имеет корней D < 0")
