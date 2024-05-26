a18f2361f163fc5866f97c43ce3a04ec7d7145c9
MackMF <160236514+MackMF@users.noreply.github.com>
2024-05-26 22:44:33 +0300

    практ 8
    й

---
+++ практ 8.py
@@ -0,0 +1,17 @@
+import numpy as np
+N=3
+A = np.array([[1, 5, 1], [4, -1, 1], [1, 6, 42]])
+B = np.array([20, 53, 120])
+x = np.linalg.inv(A).dot(B)
+print('')
+print('X')
+for i in range (0,N):
+print('%f'%x[i],end=' ')
+print('')
+x1 = np.linalg.solve(A,B)
+print('')
+print('X1')
+for i in range (0,N):
+print('%f'%x1[i],end=' ')
+print('')
+print('\nDet(A)=%f'%np.linalg.det(A))
